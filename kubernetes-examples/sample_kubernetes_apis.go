1fbcd7f8d7a5cc6d86ba99197fb00a10a51fedac
Pardhakeswar Pacha <ppacha@nvidia.com>
2020-09-11 02:07:03 -0400

    Add kubernetes-example for obtaining k8s objects using go-k8s-apis
    Signed-off-by: Pardhakeswar Pacha <ppacha@nvidia.com>

---
+++ kubernetes-examples/sample_kubernetes_apis.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"path/filepath"
+
+	//appsv1 "k8s.io/api/apps/v1"
+	//apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	//"k8s.io/client-go/util/retry"
+
+	// Uncomment to load all auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth"
+	//
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
+)
+
+func main() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Use the clientset to get the nodes or deployments
+	// and various kubernetes objects
+	fmt.Println("Clienset as been created")
+	pods, _ := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	for _, p := range pods.Items {
+		fmt.Println(p.GetName())
+	}
+}
